53f0715c07c1090775413a44beabcc79e6788133
Joel Puthankalam <jputhank@calpoly.edu>
2023-10-13 08:17:59 -0700

    schema initial commit

--- schema.sql
+++ schema.sql
@@ -9,10 +9,32 @@ CREATE TABLE
     max_potion bigint
 );
 
-for r in range(len(101)):
-    for g in range(len(101)):
-        for b in range(len(101)):
-            for d in rante(len(101)):
-                /*
-                Insert a row of this potion type
-                */ 
+
+/*
+Creating the carts table
+Fields:
+customer_name
+payment_string
+
+*/
+
+CREATE TABLE
+  potion_table (
+    id int generated always as identity not null PRIMARY KEY,
+    sku text not null,
+    name text not null, 
+    potion_type bigint,
+    quantity bigint,
+    price bigint,
+    max_potion bigint
+);
+
+
+/*
+Creating the cart_items table
+Fields:
+potion_inventory_id
+quantity
+cart_id
+
+*/
