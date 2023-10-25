043fa9f15f184735be6af049ca7890c376c709c2
Joel Puthankalam <jputhank@calpoly.edu>
2023-10-25 08:40:31 -0700

    fixed catalog duplicates

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -18,25 +18,22 @@ def get_catalog():
     with db.engine.begin() as connection:
         tab = connection.execute(sqlalchemy.text(
             """
-            SELECT potion_quantity,sku,name,price,potion_type
-            FROM(
-            SELECT COALESCE(SUM(pl.quantity),0) AS potion_quantity,sku,name,price,potion_type, 1 as priority
-            FROM potion_inventory as pi
-            LEFT JOIN potion_ledger AS pl ON pi.id = pl.potion_id
-            JOIN cart_items AS ci ON ci.potion_inventory_id = pi.id
-            JOIN carts ON carts.id = ci.cart_id
-            WHERE carts.tick = :tick and account_id = :own
-            GROUP BY pi.id,sku,name,price,potion_type
-
-            UNION
-            
-            SELECT COALESCE(SUM(pl.quantity),0) AS potion_quantity,sku,name,price,potion_type, 0 as priority
+            SELECT COALESCE(SUM(pl.quantity),0) AS potion_quantity,sku,name,price,potion_type, 
+            CASE
+            WHEN pl.potion_id IN (
+              SELECT pi.id
+              FROM potion_inventory as pi
+              LEFT JOIN potion_ledger AS pl ON pi.id = pl.potion_id
+              JOIN cart_items AS ci ON ci.potion_inventory_id = pi.id
+              JOIN carts ON carts.id = ci.cart_id
+              WHERE carts.tick = :tick and account_id = :own
+            ) THEN 1
+            ELSE 0
+            END AS priority
             FROM potion_ledger as pl
             JOIN potion_inventory ON potion_id = potion_inventory.id
             WHERE account_id = :own
             GROUP BY potion_id, sku,name,price,potion_type
-            )as subquery
-
             ORDER BY priority DESC, potion_quantity DESC
             LIMIT 6
       
