e8bbcee116150f28cfc1752352e90b898e029d69
Joel Puthankalam <jputhank@calpoly.edu>
2023-10-31 10:17:56 -0700

    fix getcurtick cursor closed

--- src/utils.py
+++ src/utils.py
@@ -19,9 +19,12 @@ BOTTLER_ID = 3
 #output: the current tick
 def getCurTick():
     with db.engine.begin() as connection:
-        tab = connection.execute(sqlalchemy.text(
+        time = connection.execute(sqlalchemy.text(
             """
             SELECT FLOOR((EXTRACT(HOUR FROM now())+2)/2) + (EXTRACT(ISODOW FROM now())-1)*12 AS tick
             """
         ))
-    return tab.scalar_one()
+    if time.closed:
+        return 0
+    else:
+        return time.scalar()
